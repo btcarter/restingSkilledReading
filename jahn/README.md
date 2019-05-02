14d09bf895b1417196bc6db7d4ac48c8e9fd8e34
btcarter <benjamin.t.carter@gmail.com>
2019-05-01 18:37:29 -0600

    adding things for seed ROI creation

---
+++ jahn/README.md
@@ -0,0 +1,4 @@
+# Notes on the "Jahn" analysis
+
+1. 7 participants (S13, S24, S27, S3, S31, S38, S6) were excluded due to motion during step 2 of the analysis. This perhaps could be rectified by relaxing motion censor parameters. I did not do this. This leaves 34 participants.
+2. Seed coordinates are -42, -44, -14 from the automated meta-analysis.
---
+++ jahn/step3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+################
+#---PREAMBLE---#
+################
+
+# Hastily written by Benjamin Carter 2019-05-01 the night before his abstract was due.
+
+# Operations: what does this script do? (aka outline of the steps and a minor explanation of why)
+# 1. Creates seed ROIs for a resting state analysis
+# 
+# Requires: what does this script require to run?
+# 1. Coordinates for your seed voxel in afni friendly RAI.
+
+###################
+#---ENVIRONMENT---#
+###################
+
+START=$(pwd) # save the directory you execute the script in.
+DEST=~/Dropbox/Lab\ data\ \&\ Papers/analyses/restingSkilledReading/jahn/VWFA # where do you want the seed mask to get sent and what shall it be named?
+REFER=~/Desktop/Luke_Reading_S11.results/ # where to refer to for dimensions
+
+###############
+#---COMMANDS--#
+###############
+
+# create tmp file with coordinates
+cd ${REFER}
+echo "-42, -44, -14" > tmp.txt
+
+# make a spherical mask
+3dUndump -prefix VWFA -master ${REFER}/errts.Luke_Reading_S11.tproject+tlrc. -srad 10 -xyz tmp.txt
