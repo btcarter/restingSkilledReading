74be4ca66590778a8bcd5b58669b51a7c509336d
btcarter <benjamin.t.carter@gmail.com>
2019-10-03 13:32:23 -0600

    fixing paths

--- example9a/get_clusters.sh
+++ example9a/get_clusters.sh
@@ -13,12 +13,9 @@ OUT_TXT=$WORK_DIR/resting_clusters.txt
 #COMMANDS#
 ##########
 
-cd $WORK_DIR
-
 #Find and printout the center of mass for all ROI's
 touch $OUT_TXT
 
-3dclust -1Dformat -orient LPI -1dindex 0 -1tindex 0 -1thresh 3 -dxyz=1 1.75 0 ${WORK_DIR}/$*.HEAD > $OUT_TXT
+3dclust -1Dformat -orient LPI -1dindex 0 -1tindex 0 -1thresh 3 -dxyz=1 1.75 0 ${WORK_DIR}/*.HEAD > $OUT_TXT
 
-cd $START_DIR
 #End
