2f33df8077b3cc95051995085a387438cffef3de
btcarter <benjamin.t.carter@gmail.com>
2019-10-04 15:54:07 -0600

    modifying to work here

--- example9a/get_participant_stats.sh
+++ example9a/get_participant_stats.sh
@@ -2,35 +2,26 @@
 #
 #
 #
-# Written by Nathan Muncy on 03/29/16, modified by Ben Carter April 23, 2018
+# Written by Nathan Muncy on 03/29/16, modified by Ben Carter October 4, 2019
 # This script requires an a priori functional mask, created via the following command:
-# 3dclust -1Dformat -nosum -1dindex 4 -1tindex 5 -2thresh -3.291 3.291 -dxyz=1 -savemask OrthoMask_mask 1.01 38 /Users/ben88/Documents/Research/fMRI_data/Reading/Compute_data/Group_Analysis1/predictability1/pred_lsa+tlrc.HEAD
+# 3dclust -1Dformat -nosum -1dindex 4 -1tindex 5 -2thresh -3.291 3.291 -dxyz=1 -savemask OrthoMask_mask 1.01 38 <results file path>
+# or by
+# 3dClusterize -nosum -1Dformat -inset ${INPUT} -idat 0 -ithr 1 -NN 1 -clust_nvox 38 -bisided -3.291 3.291 -pref_map ${PREFIX}
 
 ### set variables - these are the only things you'll have to change
 
-STUDY=/fslhome/ben88/compute/Reading
-WORK_DIR=$STUDY/Compute_data/Group_Analysis1/predictability1
-SUBJ_DIR=$STUDY/Compute_data/SubjData
+# Variables
+STUDY=${HOME}/compute/skilledReading # path to main study directory
+RESULTS=${STUDY}/resting/results # path to study results directory
+SUBJ_DIR=${STUDY}/resting # path to directory containing participant data
+STATS=${RESULTS}/part_cluster_stats # name of output file to hold the stats
+MASK=${STUDY}/clusters+tlrc # path and name of mask file (previously generated).
+SURVIVORS=${SUBJ_DIR}/survivors.txt # list of participants who survived motion censoring.
 
+touch ${STATS}.txt     # start output file
 
-### go
-
-# Lexical Stats
-
-STATS=$WORK_DIR/orthoStatsNZ
-MASK=$WORK_DIR/OrthoMask_mask+tlrc
-
-cd ${WORK_DIR}  # because who knows where you put this script
-
-touch ${STATS}.txt     # make this for later
-
-for i in $(ls $SUBJ_DIR); do
-    cd ${SUBJ_DIR}/${i}/afni_data/predictability1
-
-    stat=`3dROIstats -nzmean -nzminmax -nzsigma -mask ${MASK} '${SUBJ_DIR}/${i}/afni_data/predictability1/predictability_deconv_blur5_ANTS_resampled+tlrc[5]'`
-
-    cd ${WORK_DIR}
-
+for i in $(cat ${SURVIVORS}); do
+    stat=`3dROIstats -nzmean -nzminmax -nzsigma -mask ${MASK} '${SUBJ_DIR}/${i}/${i}.results/Luke_Reading_S4_ZMap+tlrc[0]'`
     echo "${i} ${stat}" >> ${STATS}.txt        # keep things straight
 
 done
