4ec438bb82bdcb60d584d7d61236290fe92327ee
btcarter <benjamin.t.carter@gmail.com>
2019-10-04 14:20:35 -0600

    making it work

--- example9a/trimResults.sh
+++ example9a/trimResults.sh
@@ -6,10 +6,13 @@
 #VARIABLES#
 ###########
 
-WORK_DIR=${HOME}/Box/LukeLab/SkilledReadingStudy/results/resting
-RESULTS=${WORK_DIR}/VWFA_resting+tlrc.
-TEMPLATE=${WORK_DIR}/MNI152_T1_2009c+tlrc.
-TRIMMED=${WORK_DIR}/trimmed_VWFA_resting+tlrc.
+WORK_DIR=${HOME}/Box/LukeLab/SkilledReadingStudy/results/resting/sfn2019
+RESULTS=${WORK_DIR}/VWFA_resting+tlrc.HEAD
+TEMPLATE=${WORK_DIR}/MNI152_T1_2009c+tlrc.HEAD
+TMP=${WORK_DIR}/tmp
+TRIMMED=${WORK_DIR}/trimmed_VWFA_resting+tlrc.HEAD
 
 #COMMANDS
-3dcalc -a ${TEMPLATE} -prefix ${TRIMMED} -expr "step(a)"
+3dcalc -a ${TEMPLATE} -prefix ${TMP} -expr "step(a)" # make a mask
+3dresample -master ${RESULTS} -input ${TMP}*.HEAD -prefix ${TMP}_resample # downsample to functional resolution of 3^3 voxels.
+3dcalc -a ${RESULTS} -b ${TMP}_resample*.HEAD -prefix ${TRIMMED} -expr 'a * b'  # use mask for trimming
