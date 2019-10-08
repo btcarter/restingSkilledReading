04b9fd82d2f55082b5402453c3bc348fc78cd8c8
Benjamin Carter <benjamin.t.carter@gmail.com>
2019-10-08 09:31:33 -0600

    fixing paths

--- example9a/get_participant_stats.sh
+++ example9a/get_participant_stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Written by Nathan Muncy on 03/29/16, modified by Ben Carter October 4, 2019
+# Written by Ben Carter October 4, 2019
 # This script requires an a priori functional mask, created via the following command:
 # 3dclust -1Dformat -nosum -1dindex 4 -1tindex 5 -2thresh -3.291 3.291 -dxyz=1 -savemask OrthoMask_mask 1.01 38 <results file path>
 # or by
@@ -12,14 +12,14 @@
 STUDY=${HOME}/compute/skilledReadingStudy # path to main study directory
 RESULTS=${STUDY}/resting/results # path to study results directory
 SUBJ_DIR=${STUDY}/resting # path to directory containing participant data
-STATS=${RESULTS}/part_cluster_stats # name of output file to hold the stats
+STATS=${RESULTS}/part_cluster_stats.txt # name of output file to hold the stats
 MASK=${RESULTS}/roiMask+tlrc # path and name of mask file (previously generated).
 SURVIVORS=${SUBJ_DIR}/survivors.txt # list of participants who survived motion censoring.
 
 touch ${STATS}.txt     # start output file
 
 for i in $(cat ${SURVIVORS}); do
-    stat=`3dROIstats -nzmean -nzminmax -nzsigma -mask ${MASK} '${SUBJ_DIR}/${i}/${i}.results/Luke_Reading_S4_ZMap+tlrc[0]'`
-    echo "${i} ${stat}" >> ${STATS}.txt        # keep things straight
+    stat=`3dROIstats -nzmean -nzminmax -nzsigma -mask ${MASK} ${SUBJ_DIR}/${i}/${i}.results/Luke_Reading_S4_ZMap+tlrc[0]`
+    echo "${i} ${stat}" >> ${STATS}        # keep things straight
 
 done
